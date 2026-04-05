64019c911d49406fbf4b6ac7bef6eca372d91c8b
Arturocs160 <kronculd@outlook.com>
2026-04-05 11:52:58 -0600

    Agregar esquemas para restablecer y crear nueva contraseña de usuario

--- proyecto/backend/src/schemas/userSchemas.ts
+++ proyecto/backend/src/schemas/userSchemas.ts
@@ -12,3 +12,14 @@ export const userLoginSchema = z.object({
     password: z.string("Formato inválido").min(8, "La contraseña debe tener minimo 8 carácteres").regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, "La contraseña debe contener al menos una mayúscula, una minúscula, un número y un carácter especial"),
 })
 
+export const userResetPasswordSchema = z.object({
+    email: z.email("Correo de formato inválido"),
+});
+
+export const userNewPasswordSchema = z.object({
+    id: z.uuid("ID inválido"),
+    token: z.jwt("Token inválido"),
+    newPassword: z.string().min(8, "La contraseña debe tener minimo 8 carácteres").regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, "La contraseña debe contener al menos una mayúscula, una minúscula, un número y un carácter especial"),
+});
+
+
