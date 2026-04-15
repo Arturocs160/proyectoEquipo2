c9e8e200a290563c4f87126868cd5d0a227d4de3
arturocs160 <kronculd@outlook.com>
2026-04-15 10:01:09 -0600

    fix: update CORS origin to include FRONTEND_URL from environment variables

--- proyecto/backend/src/index.ts
+++ proyecto/backend/src/index.ts
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    origin: ['http://localhost:3000', 'http://localhost:3001', process.env.FRONTEND_URL as string],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
 }));
